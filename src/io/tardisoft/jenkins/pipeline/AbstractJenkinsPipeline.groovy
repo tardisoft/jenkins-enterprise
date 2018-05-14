35319d42324558974cfeab175f961e5729db297a
coestre <christian.oestreich@optum.com>
2018-05-13 21:03:01 -0500

    With maven version

--- src/io/tardisoft/jenkins/pipeline/AbstractJenkinsPipeline.groovy
+++ src/io/tardisoft/jenkins/pipeline/AbstractJenkinsPipeline.groovy
@@ -146,7 +146,7 @@ abstract class AbstractJenkinsPipeline implements Serializable {
             }
 
             try {
-                script.withMaven("Maven 3.3.9") {
+                script.withMaven(maven: 'Maven 3.3.9') {
                     try {
                         stage(script, 'Build and Test') {
                             buildAndTest(script)
