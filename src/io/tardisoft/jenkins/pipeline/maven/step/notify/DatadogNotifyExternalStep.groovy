d9a19362d2990839a30f83692e6fe0918de61b25
coestre <christian.oestreich@optum.com>
2018-06-18 13:36:36 -0500

    Working on fix for API KEY

--- src/io/tardisoft/jenkins/pipeline/maven/step/notify/DatadogNotifyExternalStep.groovy
+++ src/io/tardisoft/jenkins/pipeline/maven/step/notify/DatadogNotifyExternalStep.groovy
@@ -13,7 +13,7 @@ class DatadogNotifyExternalStep extends AbstractNotifyExternalStep {
 
     @Override
     def notifyExternal(def script) {
-        script.withCredentials([script.usernamePassword(credentialsId: "DD_API_KEYS", usernameVariable: 'DD_API_USER', passwordVariable: 'DD_API_KEY')]) {
+        script.withCredentials([script.usernamePassword(credentialsId: "DD_API_KEYS", usernameVariable: 'API_USER', passwordVariable: 'API_KEY')]) {
             def common = new Common(script)
             script.sh("""curl  -X POST -H "Content-type: application/json" \\
     -d '{
@@ -23,7 +23,7 @@ class DatadogNotifyExternalStep extends AbstractNotifyExternalStep {
           "tags": ["org:${common.getOrg()}", "repo:${common.getRepo()}", "environment:build", "branch:${common.getBranch()}"],
           "alert_type": "info"
     }' \\
-    'https://api.datadoghq.com/api/v1/events?api_key=\${DD_API_KEY}'
+    'https://api.datadoghq.com/api/v1/events?api_key=\$API_KEY'
     """)
         }
     }
